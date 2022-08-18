f994fe06276395acc9493f256f3f41ca97753270
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-18 15:35:01 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,24 @@
+package DeconstructingMilkyWay
+
+import "fmt"
+
+package func generalCharacteristics{
+	numberPlanets = 1,000,000,000,000
+
+}
+
+func Mercury{
+
+}
+
+func Earth{}
+
+func Mars{}
+
+func Jupiter{}
+
+func Saturn{}
+
+func Uranus{}
+
+func Neptune{}
