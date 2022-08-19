e35746b5372689469e3899d03fdc7e4bd5e70b10
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-18 17:07:46 -0700

    Add files via upload

--- main.go
+++ main.go
@@ -8,14 +8,61 @@ package func generalCharacteristics{
 }
 
 func Mercury{
+	distancefromSun = 57,000,000
+	orbitSun = 29
+	radius = 1,516
+	dayTemp = 800
+	nightTemp = -290
+	largestCrater = {Name: "Caloris Basin",Diameter: 963}
+
+}
+func Venus{
+	distancefromSun = 6,724,000,000
+	radius = 6,051
+	diameter = 12,104
+	temperature = 900
+	venusCore = {Radius: 2,000}
+	density = 65,000
+
+
+}
+func Earth{
+	distancefromSun = 147,000,000
+	diameter = 7,917
+	surfaceArea = 197,000,000
+	earthAtmospheres = ["Troposphere","Stratosphere","Mesosphere","Thermosphere","Exosphere","Ionosphere"]
+	highestTemp = 110
+	lowestTemp = -126
+	earthCore = ["Crust","Asthenosphere","Rigid mantle","Stiffer mantle","Outer core (liquid)","Inner core (solid)"]
 
 }
 
-func Earth{}
+func Mars{
+	age = 4,000
+	distancefromSun = 228,000,000
+	distancefromEarth = 401,000,000
+	radius = 2,105
+	diameter = 4,212
+	mass = 6,4,000,000,000,000,000,000,000
+	density = 3,930
+	marsCore = ["Crust","Mantle","Core"]
+	impactCraters= 43,000
+	largestCanyon = "Valles Marineris"
+	firstMoon = [Name:"Phobos", Diameter: 15,500]
+	secondMoon = [Name: "Deimos", Diameter: 9,300]
 
-func Mars{}
+}
 
-func Jupiter{}
+func Jupiter{
+	distancefromSun = 483,000,000
+	radius = 4,344,000
+	diameter = 8,884,600
+	atmosphereComposition = ["Ammonia","Sulfur","Methane","Water vapor","Helium"]
+	numberMoons = 79
+	distinctFeature = {Name: "Great Red Spot", FirstObserved: 1830}
+	largestMoon = {Name: "Ganymede", Diameter: 3,273}
+
+}
 
 func Saturn{}
 
