8a9940e50c48085332d33d023041e0f83fc418a2
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-20 16:17:44 -0700

    Add files via upload

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package DeconstructingMilkyWay
 
 import "fmt"
 
-package func generalCharacteristics{
+func generalCharacteristics();
 	numberPlanets = 1,000,000,000,000
 
-}
 
-func Mercury{
+
+func Mercury();
 	distancefromSun = 57,000,000
 	orbitSun = 29
 	radius = 1,516
@@ -15,8 +15,8 @@ func Mercury{
 	nightTemp = -290
 	largestCrater = {Name: "Caloris Basin",Diameter: 963}
 
-}
-func Venus{
+
+func Venus();
 	distancefromSun = 6,724,000,000
 	radius = 6,051
 	diameter = 12,104
@@ -25,8 +25,8 @@ func Venus{
 	density = 65,000
 
 
-}
-func Earth{
+
+func Earth();
 	distancefromSun = 147,000,000
 	diameter = 7,917
 	surfaceArea = 197,000,000
@@ -35,9 +35,9 @@ func Earth{
 	lowestTemp = -126
 	earthCore = ["Crust","Asthenosphere","Rigid mantle","Stiffer mantle","Outer core (liquid)","Inner core (solid)"]
 
-}
 
-func Mars{
+
+func Mars();
 	age = 4,000
 	distancefromSun = 228,000,000
 	distancefromEarth = 401,000,000
@@ -51,9 +51,9 @@ func Mars{
 	firstMoon = [Name:"Phobos", Diameter: 15,500]
 	secondMoon = [Name: "Deimos", Diameter: 9,300]
 
-}
 
-func Jupiter{
+
+func Jupiter();
 	distancefromSun = 483,000,000
 	radius = 4,344,000
 	diameter = 8,884,600
@@ -62,10 +62,27 @@ func Jupiter{
 	distinctFeature = {Name: "Great Red Spot", FirstObserved: 1830}
 	largestMoon = {Name: "Ganymede", Diameter: 3,273}
 
-}
 
-func Saturn{}
 
-func Uranus{}
+func Saturn();
+	distancefromSun = 892,375,700
+	radius = 3,618,300
+	diameter = 7,489,700
+	temperature = -467
+	density = 0.687
+	majormoonTitan = {DiscoveredBy: "Christiaan Huygens", Year: 1655, Radius: 1,600}
+	moonHyperion = {DiscoveredBy: "William Bond, George Bond, William Lassell", Year: 1848, Radius: 3,777}
+	moonEnceladus = {DiscoveredBy: "William Herschel", Year: 1789, Radius: 155, Temperature: -324}
+	moonTethys = {DiscoveredBy: "G.D. Cassini", Year: 1684, Radius: 330}
+	moonDione = {DiscoveredBy: "G.D Cassini", Year: 1684, Radius: 348.5}
+	moonIapetus = {DiscoveredBy: "G.D Cassini", Year: 1671, Radius: 446}
 
-func Neptune{}
+
+
+func Neptune();
+	distancefromSun = 2,788,674,000
+	radius = 15,387
+	diameter = 49,244
+	temperature = -353
+	totalMoons = 14
+
